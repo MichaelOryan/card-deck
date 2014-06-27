e9ab3794d98d94de1b98285893821e62220b3cfa
Michael O'Ryan <michael.oryan@gmail.com>
2014-06-27 21:16:27 +1000

    Cleaned up layout and comments

--- include/Card.h
+++ include/Card.h
@@ -6,16 +6,24 @@
 class Card
 {
     public:
+        /* Constructors/Destructors */
         Card(const std::string &s_name);
         virtual ~Card();
+
+        /* Set values */
         void setCardName(const std::string &s_name);
-        std::string getCardName();
         void addValue(const std::string &s_key, const std::string &s_value);
+
+        /* Get Values */
+        std::string getCardName();
         std::string getValue(const std::string &s_key);
+
+        /* Overloaded operators */
         bool operator==(const Card &rhs);
 
     protected:
     private:
+        /* Values */
         std::string s_name;
         std::map<std::string, std::string> m_properties;
 };
