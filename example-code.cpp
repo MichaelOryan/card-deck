582312df00faf55b77a2b7fbab21d5520b7ea6ce
Michael O'Ryan <msor737@uowmail.edu.au>
2014-06-29 13:53:38 +1000

    More example code

--- example-code.cpp
+++ example-code.cpp
@@ -1,8 +1,8 @@
 
 
+
+//    Usage of Pile and Hand
 /*
-    Usage of Pile and Hand
-*/
     #include <Deck.h>
     #include <iostream>
 
@@ -16,6 +16,29 @@
         }
         for(Card &card : myHand){
             std::cout << card.getCardName() << "\t";
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+*/
+
+//    Usage of Deck(Pile)
+
+    #include <Deck.h>
+    #include <iostream>
+
+    int main(){
+
+        Deck deck;
+        deck.createDefaultDeck(false);
+        Deck::Hand myHand;
+        for(int i = 0; i < 26; i++){
+            myHand.push_back(deck.drawTopCard());
+        }
+        Deck halfDeck(myHand);
+        halfDeck.shufflePile();
+        while(!halfDeck.isEmpty()){
+            std::cout << halfDeck.drawTopCard().getCardName() << "\t";
         }
         std::cout << std::endl;
         return 0;
